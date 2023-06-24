160f6838e549d224a72867e8acc257095029b8b5
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-06-24 23:57:50 +0300

    HomTask002_Even

---
+++ HomTask002_Even/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая на вход принимает число и выдает,
+является ли оно четным (делится ли оно на два без остатка.)
+*/
+Console.Clear();
+Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {num},");
+if (num%2 == 0)
+{
+    Console.Write($"Число: {num} четное.");
+}
+else
+{
+    Console.Write($"Число: {num} нечетное.");
+}
