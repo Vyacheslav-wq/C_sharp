d850c03fd8108fcc5f087179f66ba7c7e7572c51
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-06-25 00:42:35 +0300

    HomTask003_EvenInInterval

---
+++ HomTask003_EvenInInterval/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+*/
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {n},");
+Console.WriteLine($"Четные числа: от 1 до {n},");
+for (int a = 2; a < (n + 1); a += 2)
+{
+    Console.Write($"{a}, ");
+}
