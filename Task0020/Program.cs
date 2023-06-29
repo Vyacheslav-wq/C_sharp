d9528aedbc9044c9bc39076dfb957924970b2b5c
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-06-29 18:25:00 +0300

    Task0017

---
+++ Task0020/Program.cs
@@ -0,0 +1,19 @@
+﻿/*# Напишите программу, которая принимает на вход координаты двух точек
+ и находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09
+A (7,-5); B (1,-1) -> 7,21
+*/
+Console.Clear();
+Console.Write("Введите x1 ");
+double x1 = Convert.ToInt64(Console.ReadLine());
+Console.Write("Введите y1 ");
+double y1 = Convert.ToInt64(Console.ReadLine());
+Console.Write("Введите x2 ");
+double x2 = Convert.ToInt64(Console.ReadLine());
+Console.Write("Введите y2 ");
+double y2 = Convert.ToInt64(Console.ReadLine());
+double d = Math.Sqrt(-5);
+
+double d = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+
+Console.WriteLine($"d ={d}");
---
+++ Task0022/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт 
+таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+*/
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= n)
+{
+    Console.Write($"{Math.Pow(count, 2)}");
+    if (count != n)
+    {
+        Console.Write(", ");
+
+    }
+    else
+        Console.Write(".");
+    count++;
+}
