dd828cc5c5977257652e6ce75ec5e2991946541a
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-06-29 18:50:15 +0300

    Task0018

--- Task0018/Program.cs
+++ Task0018/Program.cs
@@ -1,13 +1,16 @@
-﻿string QuadrantRange(int quadrant)
+﻿/* Напишите программу, которая по заданному номеру четверти, показывает
+диапазон возможных координат точек в четверти (x и y).
+*/
+Console.Clear();
+string QuadrantRange(int quadrant)
 {
-        if (quadrant == 1) return "x > 0; y > 0";
-        else if (quadrant == 2) return "x < 0; y > 0";
-        else if (quadrant == 3) return "x < 0; y < 0";
-        else if (quadrant == 4) return "x > 0; y < 0";
-        else return "invalid quadrant";
+  if (quadrant == 1) return "x > 0; y > 0";
+    else if (quadrant == 2) return "x < 0; y > 0.";
+    else if (quadrant == 3) return "x < 0; y < 0.";
+    else if (quadrant == 4) return "x > 0; y < 0.";
+    else return "Недопустимый номер четверти.";
+}
     
-    }
-    
-    Console.Write("Enter a quadrant number: ");
+    Console.Write("Введите номер четверти: ");
     int quarter = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine(QuadrantRange(quarter));
