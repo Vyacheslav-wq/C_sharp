6528251a34bf2c9d328ac719313b401c3a23b475
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-07-03 07:53:48 +0300

    Task0026

---
+++ Task0026/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Напишите программу, которая на вход принимает число и на выход
+выдает колличество цифр этого числа.*/
+Console.Clear();
+Console.Write("Введите число: ");
+
+int CountNum(int n)
+{
+    int count = 0;
+    if (n < 0)
+        n *= (-1);
+    while (n > 0)
+    {
+        n /= 10;
+        count ++;
+    }
+    return (count);
+}
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {n},");
+Console.WriteLine($"Колличество цифр в числе: {CountNum(n)}.");
+
