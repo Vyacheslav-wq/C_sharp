0c490b18201194b8914e0a07385875a1019d8c62
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-07-03 07:19:31 +0300

    Task0024

---
+++ Task0024/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая на вход принимает число А, 
+и на выход выдает сумму всех чисел от 1 го до А.*/
+
+Console.Clear();
+Console.Write("Введите число: ");
+
+int GetSum(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {n},");
+Console.WriteLine($"Сумма от 1 до {n} равна: {GetSum(n)}");
