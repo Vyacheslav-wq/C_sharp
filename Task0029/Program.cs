e630aaefbdb41225e066ec9a293181800d3b4a0e
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-07-05 20:19:35 +0300

    Task0029 и Task0030. И ни чего не понял от слова совсем.

---
+++ Task0029/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов 
+и выводит их на экран.*/
+Console.Clear();
+Console.Write("Массив Array");
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+int[] array = new int[8];
+for (uint i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-2147483648, 2147483647);
+}
+PrintArray(array);
---
+++ Task0030/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Напишите программу, которая выводит массив из 8 элементов. 
+заполненный нулями и единицами в случайном порядке.*/
+Console.Clear();
+Console.Write("Массив Array");
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length-1)
+        {
+           Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]=new Random().Next(0, 2);
+    }
+    PrintArray(array);
