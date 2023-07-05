84359ff4c169e7e5977ad7549e9c592715df35ed
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-07-05 10:33:49 +0300

    Task0027 Кое-что проясняется.

---
+++ Task0027/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт 
+сумму цифр в числе.*/
+Console.Clear();
+Console.Write("Введите число: ");
+
+int SumNum(int n)
+{
+    
+    int sum = 0;
+    while (n > 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return (sum);
+}
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {n},");
+Console.WriteLine($"Сумма цифр в числе: {SumNum(n)}.");
