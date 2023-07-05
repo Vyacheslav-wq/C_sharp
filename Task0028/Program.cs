6182a791b451a5aa7cc0db3e46bdf1e6b27fd0e9
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-07-05 21:13:51 +0300

    Task0028 Произведение от 1 до n.

---
+++ Task0028/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает число N, и выдает 
+произведение чисел от 1 до N.*/
+Console.Clear();
+Console.Write("Введите число: ");
+
+int ProductOfNum(int n)
+{
+    int pum = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        pum *= i;
+    }
+    return pum;
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine($"Вы ввели число: {n},");
+Console.WriteLine($"Произведение чисел от 1 до {n} равно: {ProductOfNum(n)}");
+
