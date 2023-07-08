6d90e0b4aaba31688d5d8d13d3009447eb375697
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-07-08 19:14:59 +0300

    Task0033 Есть или нет число в массиве.

---
+++ Task0033/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задайте массив. Напишите программу, которая определяет, присутствует ли заданное 
+число в массиве.
+*/ 
+
+int[] getRandomArray(int length, int deviation)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(-deviation, deviation + 1);
+    }
+    return result;
+}
+void printArray(int[] arrayToPrint)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write(arrayToPrint[i]);
+        if (i != arrayToPrint.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+    Console.ForegroundColor = ConsoleColor.White;
+}
+Console.WriteLine("Введите число ");
+int Num = Convert.ToInt32(Console.ReadLine());
+int[] randomArray = getRandomArray(5, 9);
+printArray(randomArray);
+bool isNumberInArray(int[] randomArray, int Number)
+{
+    for (int i = 0; i < randomArray.Length; i++)
+    {
+        if (randomArray[i] == Number)
+            return true;
+    }
+    return false;
+}
+if (isNumberInArray(randomArray, Num)) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
+
