503a32b4e5f29b152bebe4cc8b23ad60625e0d73
Vyacheslav-wq <124440610+Vyacheslav-wq@users.noreply.github.com>
2023-07-11 10:58:51 +0300

    Task0039 Разворот массива.

---
+++ Task0039/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Напишите программу, которая перевернет одномерный массив. 
+(последний элемент будет на первом месте, а первый на последнем.)
+*/
+Console.Clear();
+int[] creatingArrey(int Length, int start, int end)
+{
+    int[] array = new int[Length];
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+    return array;
+}
+void printArray(int[] array, string name = "")
+{
+    Console.WriteLine(name);
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if(i != array.Length -1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int[] inverted(int[] array)
+{
+    int[] final = new int[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    final[i] = array[array.Length-1-i];
+}
+return final;
+}
+
+int[] num = creatingArrey(10, -100, 100);
+printArray(num, "Полученный массив: ");
+int[] invArray = inverted(num);
+printArray(invArray, "Перевернутый в обратном порядке массив: ");
